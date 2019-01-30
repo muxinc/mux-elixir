9ddcbd8d845f7849b61121717c659df0ce3ce9e7
Matthew McClure <m@mmcc.io>
2019-01-29 19:29:55 -0800

    bump to 1.1.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Mux.MixProject do
   def project do
     [
       app: :mux,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
