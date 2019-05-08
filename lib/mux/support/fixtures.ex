057e47982709a88bc5aaccad03630e26c552ae6d
Jarom McDonald <jarom@vidangel.com>
2019-05-08 13:04:05 -0700

    Fix map syntax

--- lib/mux/support/fixtures.ex
+++ lib/mux/support/fixtures.ex
@@ -54,20 +54,20 @@ defmodule Mux.Fixtures do
 
   def live_stream(_) do
     %{
-      "stream_key":"54676b58-6b19-5acb-f5bf-3aa35222efc6",
-      "status":"idle",
-      "reconnect_window":60,
-      "playback_ids":[
+      "stream_key" => "54676b58-6b19-5acb-f5bf-3aa35222efc6",
+      "status" => "idle",
+      "reconnect_window" => 60,
+      "playback_ids" => [
         %{
-          "policy":"public",
-          "id":"iOkfIGstt4u1eYdFcPKGTWKz75Acpv3w"
+          "policy" => "public",
+          "id" => "iOkfIGstt4u1eYdFcPKGTWKz75Acpv3w"
         }
       ],
-      "new_asset_settings":%{
-        "playback_policies":["public"]
+      "new_asset_settings" => %{
+        "playback_policies" => ["public"]
       },
-      "id":"aA02skpHXoLrbQm49qIzAG6RtewFOcDEY",
-      "created_at":"1557338261"
+      "id" => "aA02skpHXoLrbQm49qIzAG6RtewFOcDEY",
+      "created_at" => "1557338261"
     }
   end
 
