631fe1fc2371da10721fd03b5e0213baaa8791d6
Filippo Costa <filippo.costa@protonmail.com>
2020-07-22 18:02:16 +0200

    Quality of life improvements (#20)
    * Ignore ElixirLS artifacts
    * Add support for generating signature headers
    
    Generation of signature headers can be useful to library users for
    testing purposes. Users may want to verify that their systems can
    correctly distinguish between authentic Mux webhook requests to
    malicious ones.

--- test/mux/webhooks_test.exs
+++ test/mux/webhooks_test.exs
@@ -1,18 +1,9 @@
 defmodule Mux.WebhooksTest do
   use ExUnit.Case
 
-  @scheme "v1"
   @payload "{\"test\":\"body\"}"
   @secret "SuperSecret123"
   @valid_signature_at_the_time "t=1565125718,v1=854ece4c22acef7c66b57d4e504153bc512595e8e9c772ece2a68150548c19a7"
-
-  defp generate_signature(timestamp, payload, secret) do
-    signature =
-      :crypto.hmac(:sha256, secret, "#{timestamp}.#{payload}")
-      |> Base.encode16(case: :lower)
-
-    "t=#{timestamp},#{@scheme}=#{signature}"
-  end
 
   describe "verify_header/3" do
     test "returns :error with the correct message if the signature is valid but outside the timeframe" do
@@ -31,13 +22,13 @@ defmodule Mux.WebhooksTest do
 
     test "returns :error with the correct message if the payload doesn't have a matching signature" do
       payload = "{\"test\": \"some other body\"}"
-      signature = generate_signature(System.system_time(:second), payload, @secret)
+      signature = Mux.Webhooks.TestUtils.generate_signature(payload, @secret)
       assert {:error, message} = Mux.Webhooks.verify_header(@payload, signature, @secret)
       assert message === "No signatures found matching the expected signature for payload"
     end
 
     test "returns :ok if the signature is valid" do
-      signature = generate_signature(System.system_time(:second), @payload, @secret)
+      signature = Mux.Webhooks.TestUtils.generate_signature(@payload, @secret)
       assert :ok = Mux.Webhooks.verify_header(@payload, signature, @secret)
     end
   end
