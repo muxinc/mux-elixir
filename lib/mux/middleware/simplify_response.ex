704005fa77ec422f9ab4bd2fbd3702192385ecb3
mrcasals <mrc2407@gmail.com>
2021-07-28 11:21:15 +0200

    Make middleware comply with the expected shape

--- lib/mux/middleware/simplify_response.ex
+++ lib/mux/middleware/simplify_response.ex
@@ -3,12 +3,15 @@ defmodule Mux.Middleware.SimplifyResponse do
   @behaviour Tesla.Middleware
 
   def call(env, next, _options) do
-    with {:ok, resp} <- Tesla.run(env, next),
-         %{status: status} when status >= 200 and status < 300 <- resp do
-      {:ok, get_response_contents(resp.body), resp}
-    else
-      %{body: %{"error" => %{"type" => type, "messages" => messages}}} -> {:error, type, messages}
-      err -> raise Mux.Exception, err
+    case Tesla.run(env, next) do
+      {:ok, %{status: status} = resp} when status >= 200 and status < 300 ->
+        {:ok, resp |> Map.merge(%{clean_body: get_response_contents(resp.body)})}
+
+      {:ok, %{body: %{"error" => %{"type" => type, "messages" => messages}}} = resp} ->
+        {:error, resp |> Map.merge(%{error_type: type, error_messages: messages})}
+
+      err ->
+        raise Mux.Exception, err
     end
   end
 
